5c3f06b76e1017b04c94aa6dd750710d72e68dda
ankitsingh101 <ankitsingh.udaipur@gmail.com>
2018-03-05 18:22:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 class Sidetabs extends React.Component {
   render() {
     return(
+      <div>
       <a href="#" class="list-group-item active text-center">
          <h4 class="fa fa-globe" aria-hidden="true"></h4><br>Global
       </a>
+      </div>
     );
   }
 }
