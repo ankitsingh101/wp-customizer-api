7edd69e2c924cb0c9487b994428207fe941552b4
ankitsingh101 <ankitsingh.udaipur@gmail.com>
2018-03-05 17:53:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 
 class Sidetabs extends React.Component {
- render(){
-  return (
-        <div>button</div>
-      );
+  render() {
+    return(
+      <div>
+        <h1>{this.props.name}</h1>
+        <p>
+          {this.props.bio}
+        </p>
+      </div>
+    );
   }
 }
 ReactDOM.render(<Sidetabs />, document.getElementsByClassName('list-group'));
